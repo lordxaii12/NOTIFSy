8bf934b08cd0b9432ae1003bf89a2615f029199b
lordxaii12 <ryrubio.fo10@dswd.gov.ph>
2025-05-23 16:26:59 +0800

    fixin lag issue

--- app.py
+++ app.py
@@ -101,9 +101,12 @@ def create_app():
     
     @app.after_request
     def add_header(response):
-        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
-        response.headers['Pragma'] = 'no-cache'
-        response.headers['Expires'] = '0'
+        if request.path.startswith('/static/'):
+            response.headers['Cache-Control'] = 'public, max-age=2592000, immutable'
+        else:
+            response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+            response.headers['Pragma'] = 'no-cache'
+            response.headers['Expires'] = '0'
         return response
 
     @app.before_request
